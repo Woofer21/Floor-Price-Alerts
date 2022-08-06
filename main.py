2d80cd30c3a10227b4d81593834d5a7575e552fb
Woofer21 <potatoh99@gmail.com>
2022-08-06 18:06:13 -0400

    implim settings & added dif alerts for % change

--- main.py
+++ main.py
@@ -2,12 +2,21 @@ import json
 import requests
 import datetime
 import time
-
+import math
 
 def main():
-    slug = input("Enter Slug: ")
+    with open("settings.json") as file:
+        unparsed = file.read()
+    
+    settings = json.loads(unparsed)
 
-    url = f"https://api.opensea.io/api/v1/collection/{slug}/stats"
+    SLUG = settings["settings.slug"]
+    TIME_REFRESH = settings["settings.time.refresh.seconds"]
+    TIME_PERIOD = settings["settings.time.period.minutes"]
+    PRECENT_INCREASE = settings["settings.trigger.precent.increase"]
+    PRECENT_DECREASE = settings["settings.trigger.precent.decrease"]
+
+    url = f"https://api.opensea.io/api/v1/collection/{SLUG}/stats"
 
     headers = {"Accept": "application/json"}
 
@@ -16,9 +25,6 @@ def main():
     total_change = 0
 
     while True:
-        #start_date = datetime.datetime.now().strftime("%H:%M:%S")
-        #run_time = start_date
-
         start_time = time.time()
         new_time = start_time
 
@@ -28,33 +34,38 @@ def main():
         original[0] = response["stats"]["floor_price"]
         original[1] = f"{datetime.datetime.now().replace(microsecond=0)}"
 
-        while new_time - start_time <= 600:
+        while new_time - start_time <= (TIME_PERIOD*60):
             response = requests.get(url, headers=headers)
             response = json.loads(response.text)
 
             new[0] = response["stats"]["floor_price"]
             new[1] = f"{datetime.datetime.now().replace(microsecond=0)}"
 
-            decreese = ((original[0]-new[0])/original[0]) * 100
+            difference = ((original[0]-new[0])/original[0]) * 100
 
-            total_change += decreese
+            total_change += difference
             new_time = time.time()
-            if decreese != 0.0:
+            if difference != 0.0:
                 original[0] = response["stats"]["floor_price"]
                 original[1] = f"{datetime.datetime.now().replace(microsecond=0)}"
 
 
             print(f"[ORIGINAL] {original}")
             print(f"[NEW] {new}")
-            print(f"[DECREESE] {decreese}")
+            print(f"[DECREESE] {difference}")
             print("------------")
 
-            time.sleep(30)
+            time.sleep(TIME_REFRESH)
 
-            
-        print("[INFO] 10 mins over")
-        print(f"[INFO] precent change: %{total_change}")
-        print("------------")
+        
+        if total_change > 0 and total_change > PRECENT_DECREASE:
+            print("[INFO] 10 mins over")
+            print(f"[INFO] precent - change: %{math.abs(total_change)}")
+            print("------------")
+        elif total_change < 0 and math.abs(total_change) > PRECENT_INCREASE:
+            print("[INFO] 10 mins over")
+            print(f"[INFO] precent + change: %{math.abs(total_change)}")
+            print("------------")
         original = ["", ""]
         new = ["", ""]
         total_change = 0
