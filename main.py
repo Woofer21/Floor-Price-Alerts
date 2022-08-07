c52286c67a6521b84bd828ffbf7aba504c530c74
Woofer21 <potatoh99@gmail.com>
2022-08-07 08:38:38 -0400

    ads curency rates and makes webhook a func

--- main.py
+++ main.py
@@ -39,6 +39,8 @@ def main():
     original = ["", ""]
     new = ["", ""]
     total_change = 0
+    old_fp = 0
+    new_fp = 0
 
     while True:
         start_time = time.time()
@@ -49,6 +51,7 @@ def main():
 
         original[0] = response["stats"]["floor_price"]
         original[1] = f"{datetime.datetime.now().replace(microsecond=0)}"
+        old_fp = original[0]
 
         while new_time - start_time <= (TIME_PERIOD*60):
             response = requests.get(url, headers=headers)
@@ -56,6 +59,7 @@ def main():
 
             new[0] = response["stats"]["floor_price"]
             new[1] = f"{datetime.datetime.now().replace(microsecond=0)}"
+            new_fp = new[0]
 
             difference = ((original[0]-new[0])/original[0]) * 100
 
@@ -77,6 +81,9 @@ def main():
         if total_change > 0 and total_change > PRECENT_DECREASE:
             print(f"[INFO] {TIME_PERIOD} mins over")
             print(f"[INFO] precent - change: %{math.fabs(total_change)}")
+            rate = get_rate("ETH")
+            old_usd = old_fp * rate
+            new_usd = new_fp * rate
 
             data = {
                 "username": WEBHOOK_DECREASE_NAME,
@@ -88,23 +95,32 @@ def main():
                         "description": f"The floor price has dropped over {PRECENT_DECREASE}% in the past {TIME_PERIOD} minute(s)\n\nDecreasing {round(math.fabs(total_change), 2)}%",
                         "color": 0xF94534,
                         "url": f"https://opensea.io/collection/{SLUG}",
+                        "fields": [
+                            {
+                                "name": "ETH Price",
+                                "value": f"Old Floor Price: {old_fp}\nNew Floor Price: {new_fp}",
+                                "inline": True
+                            }, 
+                            {
+                                "name": "USD Price",
+                                "value": f"Old Floor Price: {old_usd}\nNew Floor Price: {new_usd}",
+                                "inline": True
+                            }
+                        ],
                         "footer": {
                             "text": f"Collection: {SLUG}"
                         }
                     }
                 ]
             }
-            res = requests.post(WEBHOOK_DECREASE_URL, json=data)
-            try:
-                res.raise_for_status()
-            except requests.exceptions.HTTPError as err:
-                print(err)
-            else:
-                print(f"[WEB HOOK] Successfully sent, Code {res.status_code}")
-            print("------------")
+
+            send_webhook(WEBHOOK_DECREASE_URL, data)
         elif total_change < 0 and math.fabs(total_change) > PRECENT_INCREASE:
             print(f"[INFO] {TIME_PERIOD} mins over")
             print(f"[INFO] precent + change: %{math.fabs(total_change)}")
+            rate = get_rate("ETH")
+            old_usd = old_fp * rate
+            new_usd = new_fp * rate
 
             data = {
                 "username": WEBHOOK_INCREASE_NAME,
@@ -116,24 +132,32 @@ def main():
                         "description": f"The floor price has increased over {PRECENT_INCREASE}% in the past {TIME_PERIOD} minute(s)\n\nIncreasing {round(math.fabs(total_change), 2)}%",
                         "color": 0x34F950,
                         "url": f"https://opensea.io/collection/{SLUG}",
+                        "fields": [
+                            {
+                                "name": "ETH Price",
+                                "value": f"Old Floor Price: {old_fp}\nNew Floor Price: {new_fp}",
+                                "inline": True
+                            }, 
+                            {
+                                "name": "USD Price",
+                                "value": f"Old Floor Price: {old_usd}\nNew Floor Price: {new_usd}",
+                                "inline": True
+                            }
+                        ],
                         "footer": {
                             "text": f"Collection: {SLUG}"
                         }
                     }
                 ]
             }
-            res = requests.post(WEBHOOK_INCREASE_URL, json=data)
-            try:
-                res.raise_for_status()
-            except requests.exceptions.HTTPError as err:
-                print(err)
-            else:
-                print(f"[WEB HOOK] Successfully sent, Code {res.status_code}")
-            print("------------")
+            
+            send_webhook(WEBHOOK_INCREASE_URL, data)
         else:
             print(f"[INFO] {TIME_PERIOD} mins over")
             print(f"[INFO] No Change")
-
+            rate = get_rate("ETH")
+            old_usd = old_fp * rate
+            new_usd = new_fp * rate
             data = {
                 "username": WEBHOOK_NEAUTRAL_NAME,
                 "avatar_url": WEBHOOK_NEAUTRAL_AVATAR,
@@ -144,23 +168,55 @@ def main():
                         "description": f"The floor price has not made enough movement in the past {TIME_PERIOD} minutes.\n\nThe floor price moved {round(math.fabs(total_change), 2)}%",
                         "color": 0x969696,
                         "url": f"https://opensea.io/collection/{SLUG}",
+                        "fields": [
+                            {
+                                "name": "ETH Price",
+                                "value": f"Old Floor Price: {old_fp}\nNew Floor Price: {new_fp}",
+                                "inline": True
+                            }, 
+                            {
+                                "name": "USD Price",
+                                "value": f"Old Floor Price: {old_usd}\nNew Floor Price: {new_usd}",
+                                "inline": True
+                            }
+                        ],
                         "footer": {
                             "text": f"Collection: {SLUG}"
                         }
                     }
                 ]
             }
-            res = requests.post(WEBHOOK_NEAUTRAL_URL, json=data)
-            try:
-                res.raise_for_status()
-            except requests.exceptions.HTTPError as err:
-                print(err)
-            else:
-                print(f"[WEB HOOK] Successfully sent, Code {res.status_code}")
-            print("------------")
+
+            send_webhook(WEBHOOK_NEAUTRAL_URL, data)
+
         original = ["", ""]
         new = ["", ""]
         total_change = 0
+
+def get_rate(curency):
+    uri = f"https://api.coinbase.com/v2/exchange-rates?currency={curency}"
+
+    response = requests.get(uri)
+    try:
+        response.raise_for_status()
+    except requests.exceptions.HTTPError as err:
+        print(err)
+    
+    rates = json.loads(response.text)
+    usd_rate = rates["data"]["rates"]["USD"]
+    return usd_rate
+
+def send_webhook(url, data):
+    res = requests.post(url, json=data)
+    try:
+        res.raise_for_status()
+    except requests.exceptions.HTTPError as err:
+        print(err)
+    else:
+        print(f"[WEB HOOK] Successfully sent, Code {res.status_code}")
+    print("------------")
+
+#def test():
 
 if __name__ == "__main__":
     main()
